624a6334a391dfa495519c79a0dfb5391c98b30f
alfio <alfio@Omni27>
2015-09-08 19:26:26 +0200

    Base archetype

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,45 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+import javafx.application.Application;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Rectangle2D visualBounds = Screen.getPrimary().getVisualBounds();
+        double width = visualBounds.getWidth();
+        double height = visualBounds.getHeight();
+
+        Label label = new Label("Click the button.");
+        label.setTranslateY(30);
+
+        Button button = new Button("Hello JavaFXPorts");
+        button.setOnAction(e -> label.setText("Hello World!"));
+
+        Rectangle rectangle = new Rectangle(width - 20, height - 20);
+        rectangle.setFill(Color.LIGHTBLUE);
+        rectangle.setArcHeight(6);
+        rectangle.setArcWidth(6);
+
+        StackPane stackPane = new StackPane();
+        stackPane.getChildren().addAll(rectangle, button, label);
+
+        Scene scene = new Scene(stackPane, visualBounds.getWidth(), visualBounds.getHeight());
+
+        stage.setScene(scene);
+        stage.show();
+    }
+}
